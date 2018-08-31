a5e591d8ca2c2cda6a785b5d208117c3218ef889
Justin Dang <dangjustintime@gmail.com>
2018-08-31 11:27:00 -0400

    resized all icons to 25%

--- app/src/main/java/com/example/justindang/storywell/MainActivity.java
+++ app/src/main/java/com/example/justindang/storywell/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.justindang.storywell;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    // views
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.constraint_layout_anywhere) ConstraintLayout constraintLayoutAnywhere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
+        // set onClickListeners
+        constraintLayoutAnywhere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "anywehere", Toast.LENGTH_LONG).show();
+            }
+        });
         setSupportActionBar(toolbar);
     }
-
 }
