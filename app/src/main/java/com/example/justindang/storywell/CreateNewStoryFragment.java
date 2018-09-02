8ab4529cdb51851eddf5613e6b7739f99fe7da8a
Justin Dang <dangjustintime@gmail.com>
2018-09-02 15:23:42 -0400

    added some fragment layouts for choose a template, create new story, and save story. main activity onclick adds create new story fragment

---
+++ app/src/main/java/com/example/justindang/storywell/CreateNewStoryFragment.java
@@ -0,0 +1,27 @@
+package com.example.justindang.storywell;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CreateNewStoryFragment extends Fragment {
+
+
+    public CreateNewStoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_create_new_story, container, false);
+    }
+
+}
--- app/src/main/java/com/example/justindang/storywell/MainActivity.java
+++ app/src/main/java/com/example/justindang/storywell/MainActivity.java
@@ -2,29 +2,37 @@ package com.example.justindang.storywell;
 
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    // Fragment Transaction
+    FragmentTransaction fragmentTransaction;
     // views
     @BindView(R.id.toolbar) Toolbar toolbar;
     @BindView(R.id.constraint_layout_anywhere) ConstraintLayout constraintLayoutAnywhere;
+    @BindView(R.id.frame_layout_fragment_placeholder) FrameLayout frameLayoutFragmentPlaceholder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        // set onClickListeners
+        // set onClickListener
         constraintLayoutAnywhere.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "anywehere", Toast.LENGTH_SHORT).show();
+                fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.add(R.id.frame_layout_fragment_placeholder, new CreateNewStoryFragment());
+                fragmentTransaction.commit();
             }
         });
         setSupportActionBar(toolbar);
