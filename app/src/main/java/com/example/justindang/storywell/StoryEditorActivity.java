f97191003f804ef16dfa0687c389888d053fdbd7
Justin Dang <dangjustintime@gmail.com>
2018-09-09 23:14:44 -0400

    image is save to gallerygit add .

--- app/src/main/java/com/example/justindang/storywell/StoryEditorActivity.java
+++ app/src/main/java/com/example/justindang/storywell/StoryEditorActivity.java
@@ -1,7 +1,11 @@
 package com.example.justindang.storywell;
 
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -12,12 +16,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
 public class StoryEditorActivity extends AppCompatActivity {
+    // TAG
+    private static final String TAG = "StoryEditorActivity";
     // views
     @BindView(R.id.image_view_aa_icon) ImageView aaIconImageView;
     @BindView(R.id.image_view_square_circle_icon) ImageView squareCircleIconImageView;
@@ -46,8 +58,48 @@ public class StoryEditorActivity extends AppCompatActivity {
         plusIconImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // create bitmap from fragmentPlaceholderFrameLayout
+                Bitmap bitmap = Bitmap.createBitmap(
+                        fragmentPlaceholderFrameLayout.getWidth(),
+                        fragmentPlaceholderFrameLayout.getHeight(),
+                        Bitmap.Config.ARGB_8888);
+                Canvas c = new Canvas(bitmap);
+                // create file
+                String filename = "test";
+                File pictureDir = getPublicAlbumStorageDir("storywell");
+                File imageFile = new File(pictureDir,"test");
+                try {
+                    FileOutputStream outputStream = new FileOutputStream(imageFile);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                    outputStream.close();
+                    Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT);
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT);
+                    e.printStackTrace();
+                }
 
             }
         });
     }
+
+    // checks if external storage is available for read and write
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    // returns File object representing Pictures directory
+    public File getPublicAlbumStorageDir(String albumName) {
+        // get directory for the user's public pictures directory
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), albumName);
+        if (!file.mkdirs()) {
+            Log.e(TAG, "Directory not created");
+        }
+        return file;
+    };
 }
+
