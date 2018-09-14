c8788af5f322e001a8dad59b02e0c076b46730bb
Justin Dang <dangjustintime@gmail.com>
2018-09-14 11:50:22 -0400

    added template 5 layout

---
+++ app/src/main/java/com/example/justindang/storywell/Template5Fragment.java
@@ -0,0 +1,29 @@
+package com.example.justindang.storywell;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Template5Fragment extends Fragment {
+
+
+    public Template5Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_template5, container, false);
+    }
+
+}
