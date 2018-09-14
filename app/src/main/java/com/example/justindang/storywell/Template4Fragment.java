a6bddd1a995efb11ae618b4940e1fa81cb57de70
Justin Dang <dangjustintime@gmail.com>
2018-09-14 00:56:20 -0400

    added layout for template 4

---
+++ app/src/main/java/com/example/justindang/storywell/Template4Fragment.java
@@ -0,0 +1,29 @@
+package com.example.justindang.storywell;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Template4Fragment extends Fragment {
+
+
+    public Template4Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_template4, container, false);
+    }
+
+}
