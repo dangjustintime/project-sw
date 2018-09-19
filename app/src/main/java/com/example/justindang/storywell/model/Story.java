aa1efe725b1e9cfaa7ae69aad2b727a0510e65ff
Justin Dang <dangjustintime@gmail.com>
2018-09-18 21:03:28 -0400

    added addimage and removeimage to story model class

--- app/src/main/java/com/example/justindang/storywell/model/Story.java
+++ app/src/main/java/com/example/justindang/storywell/model/Story.java
@@ -5,11 +5,15 @@ import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
 public class Story {
     // member data
     private String name;
     // absolute directory path for photos
-    private String[] picturePaths;
+    private ArrayList<String> picturePaths;
     private Fragment template;
 
     // getters and setters
@@ -30,5 +34,12 @@ public class Story {
     }
     public void setTemplate(Fragment template) {
         this.template = template;
+    }
+
+    private void addImage(String imagePath) {
+      picturePaths.add(imagePath);
+    }
+    private void removeImage(String imagePath) {
+      picturePaths.remove(imagePath);
     }
 }
