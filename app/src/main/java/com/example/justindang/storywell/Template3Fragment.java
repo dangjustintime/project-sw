23a11a8c8dbd741bdbaae928ac820f071b765040
Justin Dang <dangjustintime@gmail.com>
2018-09-22 18:27:33 -0400

    implemented template 3

--- app/src/main/java/com/example/justindang/storywell/Template3Fragment.java
+++ app/src/main/java/com/example/justindang/storywell/Template3Fragment.java
@@ -1,17 +1,40 @@
 package com.example.justindang.storywell;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class Template3Fragment extends Fragment {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.app.Activity.RESULT_OK;
+
+public class Template3Fragment extends Fragment implements StoryEditorActivity.OnSaveImageListener {
+    // request codes
+    private static final int IMAGE_GALLERY_REQUEST_TOP = 29;
+    private static final int IMAGE_GALLERY_REQUEST_BOTTOM = 28;
+
+    // views
+    @BindView(R.id.image_view_template3_add_bottom_media) ImageView addBottomMediaImageView;
+    @BindView(R.id.image_view_template3_add_top_media) ImageView addTopMediaImageView;
+    @BindView(R.id.image_view_template3_bottom_media) ImageView bottomMediaImageView;
+    @BindView(R.id.image_view_template3_top_media) ImageView topMediaImageView;
+    @BindView(R.id.image_view_template3_remove_bottom_media) ImageView removeBottomMediaImageView;
+    @BindView(R.id.image_view_template3_remove_top_media) ImageView removeTopMediaImageView;
+
 
 
     public Template3Fragment() {
@@ -22,8 +45,99 @@ public class Template3Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_template3, container, false);
+        View view = inflater.inflate(R.layout.fragment_template3, container, false);
+        ButterKnife.bind(this, view);
+
+        removeTopMediaImageView.setVisibility(View.INVISIBLE);
+        removeBottomMediaImageView.setVisibility(View.INVISIBLE);
+
+        // clicklisteners
+        addTopMediaImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTopMediaImageView.setVisibility(View.INVISIBLE);
+                Intent photoGalleryIntent = new Intent(Intent.ACTION_PICK);
+                File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                String pictureDirectoryPath = pictureDirectory.getPath();
+                Uri data = Uri.parse(pictureDirectoryPath);
+                photoGalleryIntent.setDataAndType(data, "image/*");
+                startActivityForResult(photoGalleryIntent, IMAGE_GALLERY_REQUEST_TOP);
+                removeTopMediaImageView.setVisibility(View.VISIBLE);
+            }
+        });
+        addBottomMediaImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addBottomMediaImageView.setVisibility(View.INVISIBLE);
+                Intent photoGalleryIntent = new Intent(Intent.ACTION_PICK);
+                File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                String pictureDirectoryPath = pictureDirectory.getPath();
+                Uri data = Uri.parse(pictureDirectoryPath);
+                photoGalleryIntent.setDataAndType(data, "image/*");
+                startActivityForResult(photoGalleryIntent, IMAGE_GALLERY_REQUEST_BOTTOM);
+                removeBottomMediaImageView.setVisibility(View.VISIBLE);
+            }
+        });
+        removeTopMediaImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                topMediaImageView.setImageBitmap(null);
+                addTopMediaImageView.setVisibility(View.VISIBLE);
+                removeTopMediaImageView.setVisibility(View.INVISIBLE);
+            }
+        });
+        removeBottomMediaImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomMediaImageView.setImageBitmap(null);
+                addBottomMediaImageView.setVisibility(View.VISIBLE);
+                removeBottomMediaImageView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        return view;
     }
 
+    // return selected image from gallery
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == IMAGE_GALLERY_REQUEST_TOP) {
+                Uri imageUri = data.getData();
+                InputStream inputStream;
+                try {
+                    inputStream = getContext().getContentResolver().openInputStream(imageUri);
+                    Bitmap imageBitmap = BitmapFactory.decodeStream(inputStream);
+                    topMediaImageView.setScaleType(ImageView.ScaleType.MATRIX);
+                    topMediaImageView.setImageBitmap(imageBitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            } else if (requestCode == IMAGE_GALLERY_REQUEST_BOTTOM) {
+                Uri imageUri = data.getData();
+                InputStream inputStream;
+                try {
+                    inputStream = getContext().getContentResolver().openInputStream(imageUri);
+                    Bitmap imageBitmap = BitmapFactory.decodeStream(inputStream);
+                    bottomMediaImageView.setScaleType(ImageView.ScaleType.MATRIX);
+                    bottomMediaImageView.setImageBitmap(imageBitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public void hideUI() {
+        removeBottomMediaImageView.setVisibility(View.INVISIBLE);
+        removeTopMediaImageView.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void showUI() {
+
+    }
 }
